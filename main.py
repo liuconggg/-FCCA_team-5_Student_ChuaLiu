19f93a16e970adafcdc822e6fd61b499dca05df9
liuconggg <128717856+liuconggg@users.noreply.github.com>
2024-04-04 20:21:46 +0800

    created dataframe

--- main.py
+++ main.py
@@ -1,3 +1,29 @@
-print("Hello World")
+import numpy as np
+import pandas as pd
+import seaborn as sb
+import matplotlib.pyplot as plt
 
-print("added by test")
+
+sb.set_theme() 
+
+jobData = pd.read_csv('job_placement.csv')
+uniRankings = pd.read_csv('university_rankings.csv')
+
+new_header = uniRankings.iloc[0] #grab the first row for the header
+uniRankings = uniRankings[1:] #take the data less the header row
+uniRankings.columns = new_header #set the header row as the df header
+
+jobData.head()
+
+uniRankings.head(10)
+
+uniRankings.drop("2023", axis=1)
+
+
+
+# Markdown 1: Cleaning of Data 
+jobData.drop("name", axis=1) # dropping column "name"
+collegeName = jobData["college_name"].unique()
+print((collegeName))
+
+
