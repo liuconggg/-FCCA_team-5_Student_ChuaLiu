fe4223d02c6c3c9dcddb492a0631dc1009d71d66
liuconggg <128717856+liuconggg@users.noreply.github.com>
2024-04-05 11:47:34 +0800

    Data preparation by including uni rankings and focusing on CS and IT courses

--- main.py
+++ main.py
@@ -2,28 +2,47 @@ import numpy as np
 import pandas as pd
 import seaborn as sb
 import matplotlib.pyplot as plt
-
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 
 sb.set_theme() 
 
 jobData = pd.read_csv('job_placement.csv')
-uniRankings = pd.read_csv('university_rankings.csv')
-
-new_header = uniRankings.iloc[0] #grab the first row for the header
-uniRankings = uniRankings[1:] #take the data less the header row
-uniRankings.columns = new_header #set the header row as the df header
+uniRankings = pd.read_csv('uni_ranking.csv')
 
 jobData.head()
-
-uniRankings.head(10)
-
-uniRankings.drop("2023", axis=1)
-
+uniRankings.head()
 
 
 # Markdown 1: Cleaning of Data 
-jobData.drop("name", axis=1) # dropping column "name"
-collegeName = jobData["college_name"].unique()
-print((collegeName))
+jobData = jobData.drop("name", axis=1) # dropping column "name"
+jobData = jobData.replace("--", "" ,regex = True)
 
+collegeName = jobData["college_name"].unique() # find out how many unique college
+uniRanking = pd.DataFrame(uniRankings[['2024 RANK', 'Institution Name']]).iloc[1:] # extra only '2024 RANK' and 'Instituition Name' from row 1 onwards
+cleanedUniRankings = uniRanking.set_index('2024 RANK') 
+uniRankingDict = cleanedUniRankings.to_dict('dict') # convert to dictionary for comparison later on
+uniRankingDict= uniRankingDict['Institution Name'] # accessing the dictionary 
+
+
+def find_best_match(college_name, uniRankingDict):
+    """Finds the best match and returns the corresponding ranking"""
+    choices = list(uniRankingDict.values())  # Use values as choices
+    best_match = process.extractOne(college_name, choices, scorer=fuzz.ratio)
+    if best_match[1] >= 80: # ratio of 80 and above 
+        for ranking, uni_name in uniRankingDict.items():
+            if uni_name == best_match[0]:
+                return ranking
+    else:
+        return None  # Or a default value for no match
+
+# Create new column 
+jobData['ranking'] = jobData['college_name'].apply(lambda x: find_best_match(x, uniRankingDict))
+
+jobData.head()
+
+courses = ['Computer Science', 'Information Technology'] 
+
+jobData = jobData[jobData['stream'].isin(courses)] # extra only computer science and infromation technology
+print(jobData)
 
