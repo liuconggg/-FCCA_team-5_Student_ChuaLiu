9e7fedf61b2ddf95d2b616411ff8f7bace53b3d0
Anthony Chua <anthonychua@Anthonys-MacBook-Pro-4.local>
2024-04-18 15:32:49 +0800

    Remove added imports

--- main.py
+++ main.py
@@ -5,10 +5,6 @@ import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from collections import defaultdict
 
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-
-
 sb.set_theme() 
 
 jobData = pd.read_csv('job_placement.csv')
